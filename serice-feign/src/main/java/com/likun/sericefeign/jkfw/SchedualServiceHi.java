81788c2646eaf43bf3dd22a57f3d66d92718221d
test1 <test1>
2018-07-24 18:47:27 +0800

    springcloud 20180724 学习

---
+++ serice-feign/src/main/java/com/likun/sericefeign/jkfw/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package com.likun.sericefeign.jkfw;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+/**
+ * 定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务。比如在代码中调用了service-hi服务的“/hi”接口
+ *
+ * */
+@Service
+@FeignClient(value = "eureka-server2")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ service-ribbon/src/main/java/com/likun/serviceribbon/service/HelloService.java
@@ -0,0 +1,28 @@
+package com.likun.serviceribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+    /**
+     * 通过之前注入ioc容器的restTemplate来消费service-hi服务的“/hi”接口，
+     * 在这里我们直接用的程序名替代了具体的url地址，
+     * 在ribbon中它会根据服务名来选择具体的服务实例，
+     * 根据服务实例在请求的时候会用具体的url替换掉服务名
+     * */
+
+    /**
+     * 一个服务注册中心，eureka server,端口为8761
+     * service-hi工程跑了两个实例，端口分别为8762,8763，分别向服务注册中心注册
+     * sercvice-ribbon端口为8764,向服务注册中心注
+     * 当sercvice-ribbon通过restTemplate调用service-hi的hi接口时，因为用ribbon进行了负载均衡，
+     * 会轮流的调用service-hi：8762和8763 两个端口的hi接口；
+     * */
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://eureka-server2/hi?name="+name,String.class);
+    }
+}
